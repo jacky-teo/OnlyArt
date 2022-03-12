79b7bae885676b181fc0b0faeb15c6a83ada686e
jacky-teo <jackytjq@outlook.com>
2022-03-13 01:21:38 +0800

    worked on postcontent,viewcontent & creatorcontent

--- services/microservice/creator_content.py
+++ services/microservice/creator_content.py
@@ -1,174 +1,157 @@
-#!/usr/bin/env python3
-# The above shebang (#!) operator tells Unix-like environments
-# to run this file as a python3 script
-
-import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/onlyfence'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/book'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
 
 db = SQLAlchemy(app)
 
+CORS(app)  
 
-class Order(db.Model):
-    __tablename__ = 'creator_content'
+class CREATOR_CONTENT(db.Model):
+    __tablename__ = 'CREATOR_CONTENT'
 
-    POSTID = db.Column(db.Integer, primary_key=True)
-    CREATORID = db.Column(db.String(32), nullable=False)
-    DESCRIPTION = db.Column(db.String(64), nullable=False)
-    IMAGE = db.Column(db.Blob(10), nullable=False)
-    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    modified = db.Column(db.DateTime, nullable=False,
-                         default=datetime.now, onupdate=datetime.now)
+    POSTID = db.Column(db.String(13), primary_key=True)
+    CREATORID = db.Column(db.String(64), nullable=False)
+    DESCRIPTION = db.Column(db.Float(precision=2), nullable=False)
+    IMAGE = db.Column(db.BLOB(length=None), nullable=False)
+    POST_DATE = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    modified = db.Column(db.DateTime, nullable=False,default=datetime.now, onupdate=datetime.now)
 
-    def json(self):
-        dto = {
-            'order_id': self.order_id,
-            'customer_id': self.customer_id,
-            'status': self.status,
-            'created': self.created,
-            'modified': self.modified
-        }
-
-        dto['order_item'] = []
-        for oi in self.order_item:
-            dto['order_item'].append(oi.json())
-
-        return dto
-
-
-class Order_Item(db.Model):
-    __tablename__ = 'order_item'
-
-    item_id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.ForeignKey(
-        'order.order_id', ondelete='CASCADE', onupdate='CASCADE'), nullable=False, index=True)
-
-    book_id = db.Column(db.String(13), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-
-    # order_id = db.Column(db.String(36), db.ForeignKey('order.order_id'), nullable=False)
-    # order = db.relationship('Order', backref='order_item')
-    order = db.relationship(
-        'Order', primaryjoin='Order_Item.order_id == Order.order_id', backref='order_item')
+    def __init__(self, POSTID, CREATORID, DESCRIPTION, IMAGE,POST_DATE,modified):
+        self.POSTID = POSTID
+        self.CREATORID = CREATORID
+        self.DESCRIPTION = DESCRIPTION
+        self.IMAGE = IMAGE
+        self.POST_DATE = POST_DATE
+        self.modified = modified
 
     def json(self):
-        return {'item_id': self.item_id, 'book_id': self.book_id, 'quantity': self.quantity, 'order_id': self.order_id}
+        return {"POSTID": self.POSTID, "CREATORID": self.CREATORID, "DESCRIPTION": self.DESCRIPTION, "IMAGE": self.IMAGE,"POST_DATE": self.POST_DATE,"modified": self.modified}
 
 
-@app.route("/order")
+@app.route("/creator_content")
 def get_all():
-    orderlist = Order.query.all()
-    if len(orderlist):
+    content_list = CREATOR_CONTENT.query.all()
+    if len(content_list):
         return jsonify(
             {
                 "code": 200,
                 "data": {
-                    "orders": [order.json() for order in orderlist]
+                    "contents": [content.json() for content in content_list]
                 }
             }
         )
     return jsonify(
         {
             "code": 404,
-            "message": "There are no orders."
+            "message": "There are no content."
         }
     ), 404
 
 
-@app.route("/order/<string:order_id>")
-def find_by_order_id(order_id):
-    order = Order.query.filter_by(order_id=order_id).first()
-    if order:
+@app.route("/creator_content/<string:creatorid>")
+def find_by_creatorid(creatorid):
+    content_list = CREATOR_CONTENT.query.filter_by(creatorid=creatorid).first()
+    if len(content_list):
         return jsonify(
             {
                 "code": 200,
-                "data": order.json()
+                "data": {
+                    "contents": [content.json() for content in content_list]
+                }
             }
         )
     return jsonify(
         {
             "code": 404,
-            "data": {
-                "order_id": order_id
-            },
-            "message": "Order not found."
+            "message": "There are no content for this creator."
         }
     ), 404
 
 
-@app.route("/order", methods=['POST'])
-def create_order():
-    customer_id = request.json.get('customer_id', None)
-    order = Order(customer_id=customer_id, status='NEW')
+@app.route("/creator_content/<string:creatorid>", methods=['POST'])
+def upload_content(creatorid):
 
-    cart_item = request.json.get('cart_item')
-    for item in cart_item:
-        order.order_item.append(Order_Item(
-            book_id=item['book_id'], quantity=item['quantity']))
+    data = request.get_json()
+    content = CREATOR_CONTENT(**data)
 
     try:
-        db.session.add(order)
+        db.session.add(content)
         db.session.commit()
-    except Exception as e:
+    except:
         return jsonify(
             {
                 "code": 500,
-                "message": "An error occurred while creating the order. " + str(e)
+                "data": {
+                    "CreatorID": creatorid
+                },
+                "message": "An error occurred inserting the content."
             }
         ), 500
 
     return jsonify(
         {
             "code": 201,
-            "data": order.json()
+            "data": content.json()
         }
     ), 201
 
 
-@app.route("/order/<string:order_id>", methods=['PUT'])
-def update_order(order_id):
-    try:
-        order = Order.query.filter_by(order_id=order_id).first()
-        if not order:
-            return jsonify(
-                {
-                    "code": 404,
-                    "data": {
-                        "order_id": order_id
-                    },
-                    "message": "Order not found."
-                }
-            ), 404
-
-        # update status
+@app.route("/content_creator/<string:contentID>", methods=['PUT'])
+def update_content(contentID):
+    content = CREATOR_CONTENT.query.filter_by(contentID=contentID).first()
+    if content:
         data = request.get_json()
-        if data['status']:
-            order.status = data['status']
-            db.session.commit()
-            return jsonify(
-                {
-                    "code": 200,
-                    "data": order.json()
-                }
-            ), 200
-    except Exception as e:
+        if data['IMAGE']:
+            content.IMAGE = data['IMAGE']
+        if data['DESCRIPTION']:
+            content.DESCRIPTION = data['DESCRIPTION']
+        db.session.commit()
         return jsonify(
             {
-                "code": 500,
-                "data": {
-                    "order_id": order_id
-                },
-                "message": "An error occurred while updating the order. " + str(e)
+                "code": 200,
+                "data": content.json()
             }
-        ), 500
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "data": {
+                "contentID": contentID
+            },
+            "message": "Content not found."
+        }
+    ), 404
+
+
+@app.route("/content_creator/<string:contentID>", methods=['DELETE'])
+def delete_book(contentID):
+    book = CREATOR_CONTENT.query.filter_by(contentID=contentID).first()
+    if book:
+        db.session.delete(book)
+        db.session.commit()
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "contentID": contentID + ' has been deleted'
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "data": {
+                "contentID": contentID
+            },
+            "message": "Content not found."
+        }
+    ), 404
 
 
 if __name__ == '__main__':
-    print("This is flask for " + os.path.basename(__file__) + ": manage orders ...")
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(port=5000, debug=True)
