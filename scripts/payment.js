3e4cd430370607c0b64403b48d7e77ebbc71fdbd
Russell <russelltiojh@gmail.com>
2022-03-12 19:05:49 +0800

    Updated documentation

--- scripts/payment.js
+++ scripts/payment.js
@@ -1,13 +1,23 @@
 // For Paypal Javascript SDK
 
+// Paypal Developer Dashboard: https://developer.paypal.com/developer/applications 
+// Sandbox PayPal Environment: https://www.sandbox.paypal.com/ 
+    // Use this to check sandbox account
+
 // Sandbox Personal Account
 // Email: sb-wptk414408488@personal.example.com
 // Pass: {vg<>1DI
 
 // Sandbox Client ID of a REST app
 // Email: sb-vrehs14346230@business.example.com
+// Pass: AYip4<3e
 // Client ID: Afx50ZFn0R7g2tyN0P08kc3fBR0Csy8w1J25MND90MVCnpbLwiaIS-UiNElzqypPKulongQDAcq41D0M
 // This App type is: Platform
 
 // Resources
-// https://developer.paypal.com/docs/checkout/standard/integrate/
+// PayPal Standard Integration: https://developer.paypal.com/docs/checkout/standard/integrate/
+// JS SDK: https://developer.paypal.com/sdk/js/configuration/ 
+    // See "Complete Reference" > Buttons: This allows us to modify the behaviour of the PayPal button
+// Paypal Multiparty Payments (for Platform App type): https://developer.paypal.com/docs/multiparty/
+    // Integrate sellers before payment: https://developer.paypal.com/docs/multiparty/seller-onboarding/before-payment/ 
+    // Issue Refunds from Seller to Buyer: https://developer.paypal.com/docs/multiparty/issue-refund/ 
