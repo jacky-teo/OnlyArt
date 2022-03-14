0c06968ef9c31cfae4039993c1d9873c24d48ea0
shyatup <quahshyawei@gmail.com>
2022-03-15 03:12:50 +0800

    Add database connection for consumer_account.py and creator_account.py

--- services/microservice/consumer_account.py
+++ services/microservice/consumer_account.py
@@ -1,1 +1,33 @@
 ## Microservice to store consumer information ##
+from audioop import add
+from http.client import CREATED
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@localhost:3306/onlyfence'
+## root@localhost will change
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+db = SQLAlchemy(app)
+CORS(app)
+
+class consumerAccount(db.Model): 
+    __tablename__ = "consumeraccount"
+
+    CONSUMERID = db.Column(db.String(64), primary_key=True, nullable=False)
+    USERNAME = db.Column(db.String(64), nullable=False)
+    PASSWORD = db.Column(db.String(64), nullable=False)
+    TELEGRAM = db.Column(db.String(64), nullable=False)
+
+    def __init__(self, CONSUMERID, USERNAME, PASSWORD, TELEGRAM):
+        self.CONSUMERID = CONSUMERID
+        self.USERNAME = USERNAME
+        self.PASSWORD = PASSWORD
+        self.TELEGRAM = TELEGRAM
+
+    def json(self):
+        return {"CONSUMERID": self.CONSUMERID, "USERNAME": self.USERNAME, "PASSWORD": self.PASSWORD, "TELEGRAM": self.TELEGRAM}
+
--- services/microservice/creator_account.py
+++ services/microservice/creator_account.py
@@ -1,1 +1,34 @@
 ## Microservice to store creator account information ##
+from audioop import add
+from http.client import CREATED
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@localhost:3306/onlyfence'
+## root@localhost will change
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+db = SQLAlchemy(app)
+CORS(app)
+
+class creatorAccount(db.Model): 
+    __tablename__ = "creatoraccount"
+
+    CREATORID = db.Column(db.String(64), primary_key=True, nullable=False)
+    USERNAME = db.Column(db.String(64), nullable=False)
+    PASSWORD = db.Column(db.String(64), nullable=False)
+    EMAIL = db.Column(db.String(64), nullable=False)
+    PRICE = db.Column(db.Float(precision=2), nullable=False)
+
+    def __init__(self, CREATORID, USERNAME, PASSWORD, EMAIL, PRICE):
+        self.CREATORID = CREATORID
+        self.USERNAME = USERNAME
+        self.PASSWORD = PASSWORD
+        self.EMAIL = EMAIL
+        self.PRICE = PRICE
+
+    def json(self):
+        return {"CREATORID": self.CREATORID, "USERNAME": self.USERNAME, "PASSWORD": self.PASSWORD, "EMAIL": self.EMAIL,"PRICE": self.PRICE}
