bc2c02539781b6d90de6bc4595299543eeab6aca
shyatup <quahshyawei@gmail.com>
2022-03-15 12:34:59 +0800

    Add scenario 1 and 4 to creator_account.py

--- services/microservice/creator_account.py
+++ services/microservice/creator_account.py
@@ -4,17 +4,16 @@ from http.client import CREATED
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@localhost:3306/onlyfence'
-## root@localhost will change
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/onlyfence'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 CORS(app)
 
-class creatorAccount(db.Model): 
+
+class creatorAccount(db.Model):
     __tablename__ = "creatoraccount"
 
     CREATORID = db.Column(db.String(64), primary_key=True, nullable=False)
@@ -31,7 +30,55 @@ class creatorAccount(db.Model):
         self.PRICE = PRICE
 
     def json(self):
-        return {"CREATORID": self.CREATORID, "USERNAME": self.USERNAME, "PASSWORD": self.PASSWORD, "EMAIL": self.EMAIL,"PRICE": self.PRICE}
+        return {"CREATORID": self.CREATORID, "USERNAME": self.USERNAME, "PASSWORD": self.PASSWORD, "EMAIL": self.EMAIL, "PRICE": self.PRICE}
+
+# scenario 1
+
+
+@app.route('/creatorprice')
+def get_creator_price():
+    creatorid = request.args.get('CREATORID', None)
+    status = creatorAccount.query.filter_by(
+        CREATORID=creatorid).first()
+
+    if (status):
+        return jsonify(
+            {
+                "code": 200,
+                "data": status.PRICE
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Creator does not exist."
+        }
+    ), 404
+
+# scenario 4
+
+
+@app.route('/creatorid')
+def get_creator_id():
+    creatorid = request.args.get('CREATORID', None)
+    status = creatorAccount.query.filter_by(
+        CREATORID=creatorid).first()
+
+    if (status):
+        return jsonify(
+            {
+                "code": 200,
+                "data": status.CREATORID
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Creator does not exist."
+        }
+    ), 404
+
+# missing scenario 2
 
 
 if __name__ == '__main__':
