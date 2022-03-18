6da27e1d2ab1a4dcbb04a91392403040fd9a851c
LIN SHUYI <shuyi.lin.2020@scis.smu.edu.sg>
2022-03-19 00:54:10 +0800

    notification module with telegram bot api
    telegram bot key is hidden in .env

--- services/notification.py
+++ services/notification.py
@@ -1,1 +1,146 @@
 ## Microservice to work with telegram API ##
+import requests
+import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+from os import environ
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/notification'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+token = os.getenv('TELEGRAM_KEY')
+
+db = SQLAlchemy(app)
+
+class Notification(db.Model):
+    __tablename__ = 'notification'
+
+    chatid = db.Column(db.String(64), primary_key=True)
+    userid = db.Column(db.String(64), nullable=False)
+
+    def __init__(self, chatid, userid):
+        self.chatid = chatid
+        self.userid = userid
+
+    def json(self):
+        return {"chatid": self.chatid, "userid": self.userid}
+
+
+@app.route("/chat")
+def get_all():
+    chatlist = Notification.query.all()
+    if len(chatlist):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "chats": [chat.json() for chat in chatlist]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no chat ids."
+        }
+    ), 404
+
+
+@app.route("/chat/<string:userid>")
+def find_by_isbn13(userid):
+    notif = Notification.query.filter_by(userid=userid).first()
+    if notif:
+        return jsonify(
+            {
+                "code": 200,
+                "data": notif.json()
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Notification information not found."
+        }
+    ), 404
+
+
+@app.route("/chat/<string:userid>", methods=['POST'])
+def create_notif(userid):
+    if (Notification.query.filter_by(userid=userid).first()):
+        return jsonify(
+            {
+                "code": 400,
+                "data": {
+                    "userid": userid
+                },
+                "message": "Notification info already exists."
+            }
+        ), 400
+
+    data = request.get_json()
+    print(data['chatid'])
+    notif = Notification(data['chatid'],userid)
+
+    try:
+        db.session.add(notif)
+        db.session.commit()
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "data": {
+                    "userid": userid
+                },
+                "message": "An error occurred creating the notif."
+            }
+        ), 500
+
+    return jsonify(
+        {
+            "code": 201,
+            "data": notif.json()
+        }
+    ), 201
+
+@app.route("/notify/<string:creatorname>",methods=["POST"])
+def send_notif(creatorname):
+    #some function here
+    countnotif = 0
+    data = request.get_json()
+    #print("creatorname: ",creatorname)
+    #print("data: ",data)
+    notiflist = data["list"]
+    #print("NL: ",notiflist)
+    for user in notiflist:
+        #request send
+        #print("user: ",user)
+        if (Notification.query.filter_by(userid=user).first()):
+            notif = Notification.query.filter_by(userid=user).first()
+            notifinfo = notif.json()
+            chatid = notifinfo["chatid"]
+            #print("chatid: ",chatid)
+            chatmsg = "Creator " + creatorname + " has posted! Check it out on OnlyFences now!"
+            #chatparams = {'chat_id': chatid , 'text': chatmsg}
+            sendurl = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chatid + "&text=" + chatmsg 
+            r = requests.get(sendurl)
+            countnotif += 1
+    if countnotif == 0:        
+        return jsonify(
+                {
+                    "code": 500,
+                    "message": "Notification was unsuccessful. No notifs sent."
+                }
+            ), 500
+    else:
+        successmsg = "Notification was successful." + countnotif + " number of notifs were sent."
+        return jsonify(
+                {
+                    "code": 201,
+                    "message": successmsg
+                }
+            ), 201
+
+
+if __name__ == '__main__':
+     app.run(host='127.0.0.1', port=5000, debug=True)
---
+++ services/telebot.py
@@ -0,0 +1,30 @@
+from telegram.ext import Updater, CommandHandler
+import logging
+import os
+import sqlalchemy as db
+
+teletoken = os.getenv('TELEGRAM_KEY')
+engine = db.create_engine('mysql+mysqlconnector://root:root@localhost:3306/notification')
+connection = engine.connect()
+metadata = db.MetaData()
+notification = db.Table('notification', metadata, autoload=True, autoload_with=engine)
+
+
+# writting functionality of the command
+def start(update, context):
+    user_id = update.message.from_user.id
+    user_name = update.message.from_user.name
+    print(user_id,user_name)
+    query = db.insert(notification).values(chatid=user_id, userid=user_name) 
+    ResultProxy = connection.execute(query) 
+    message = 'Welcome to the bot',user_name   
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+updater = Updater(token=teletoken, use_context=True)
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+# give a name to the command and add it to the dispaatcher
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+updater.start_polling()
