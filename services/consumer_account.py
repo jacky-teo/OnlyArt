66b288294a89ae3d569700fe03c5a227cb42257a
LIN SHUYI <shuyi.lin.2020@scis.smu.edu.sg>
2022-03-19 15:05:10 +0800

    some edits wheee
    consumer account edited and notif data["list"] changed to data["data"]

--- services/consumer_account.py
+++ services/consumer_account.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/onlyfence'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/onlyfence'
 # root@localhost will change
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -34,20 +34,26 @@ class consumerAccount(db.Model):
 @app.route("/consumer/retrievetelegram")
 def get_telegram():
     data = request.get_json() 
+    count=0
+    consumerlist = []
     consumers = data["data"]
 
     for consumer in consumers:
         response = consumerAccount.query.filter_by(CONSUMERID=consumer).first()
         if response:
             # if consumer is present
-            return jsonify({
-                "code": 200,
-                "data": response.TELEGRAM
-            }), 200
+            consumerlist.append(response.TELEGRAM)
+    if len(consumerlist)== 0:
         return jsonify({
             "code": 404,
-            "message": "Consumer does not exist."
+            "message": "No consumer telegram tags were found."
         }), 404
+    else:
+        return jsonify({
+                "code": 200,
+                "data": consumerlist
+            }), 200
+        
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
