2429afacc9e92f2aaa9a7086e50d96504fecab13
BryanCh0ng <90763256+BryanCh0ng@users.noreply.github.com>
2022-03-22 17:39:36 +0800

    Add files via upload

--- databases/activity.sql
+++ databases/activity.sql
@@ -1,6 +1,15 @@
 -- Database: `activity`
 --
-CREATE DATABASE IF NOT EXISTS ACTIVITY;
-USE ACTIVITY;
 
--- incomplete sql filee
+DROP DATABASE IF EXISTS `ACTIVITY`;
+CREATE DATABASE IF NOT EXISTS `ACTIVITY` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `ACTIVITY`;
+
+CREATE TABLE IF NOT EXISTS `ACTIVITY` (
+  `log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(64) NOT NULL,
+  `log_content` varchar(500) NOT NULL,
+  `log_from` varchar(128) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  PRIMARY KEY (`log_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
