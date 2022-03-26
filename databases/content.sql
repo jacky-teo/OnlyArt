d6a531f6e2afdd86590982825c214533c8973e11
jacky-teo <jackytjq@outlook.com>
2022-03-26 12:09:45 +0800

    editted dummy data

--- databases/content.sql
+++ databases/content.sql
@@ -26,10 +26,10 @@ CREATE TABLE CONTENT(
 
 INSERT INTO CONTENT(POSTID,CREATORID,DESCRIPTION,IMAGE_ID,IMG_EXT) 
 VALUES 
-('CR001_IMG1','CR001', 'this is img1', "img1",'png'),
-('CR001_IMG2','CR001', 'this is img2', "img2",'png'),
-('CR001_IMG3','CR001', 'this is img3', "img3",'png'),
-('CR001_IMG4','CR001', 'this is img4', "img4",'png')
+('CR001_IMG11','CR001', 'this is img1', "img11",'png'),
+('CR001_IMG12','CR001', 'this is img2', "img12",'png'),
+('CR001_IMG13','CR001', 'this is img3', "img13",'png'),
+('CR001_IMG14','CR001', 'this is img4', "img14",'png')
 -- ('CR002_IMG1','CR002', 'this is cr2img1', 'cr2img1'),
 -- ('CR002_IMG2','CR002', 'this is cr2img2', 'cr2img2'),
 -- ('CR002_IMG3','CR002', 'this is cr2img3', 'cr2img3');
