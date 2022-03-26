5e578fc9c8b0ccf7281591d9216cc995094e1e8c
shyatup <quahshyawei@gmail.com>
2022-03-26 12:20:29 +0800

    Add generic topic for all complex MS to use

--- services/amqp_setup.py
+++ services/amqp_setup.py
@@ -1,61 +1,71 @@
 import pika
 
-hostname = "localhost" # default hostname
-port = 5672 # default port
+hostname = "localhost"  # default hostname
+port = 5672  # default port
 # connect to the broker and set up a communication channel in the connection
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(
         host=hostname, port=port,
-        heartbeat=3600, blocked_connection_timeout=3600, # these parameters to prolong the expiration time (in seconds) of the connection
-))
+        # these parameters to prolong the expiration time (in seconds) of the connection
+        heartbeat=3600, blocked_connection_timeout=3600,
+    ))
 
 channel = connection.channel()
 # Set up the exchange if the exchange doesn't exist
 # - use a 'topic' exchange to enable interaction
-exchangename="post_topic"
-exchangetype="topic"
-channel.exchange_declare(exchange=exchangename, exchange_type=exchangetype, durable=True)
-    # 'durable' makes the exchange survive broker restarts
+
+############   Reusable Topic   #############
+exchangename = "generic_topic"
+exchangetype = "topic"
+channel.exchange_declare(exchange=exchangename,
+                         exchange_type=exchangetype, durable=True)
+# 'durable' makes the exchange survive broker restarts
 
 ############   Error queue   #############
-#delcare Error queue
+# delcare Error queue
 queue_name = 'Error'
-channel.queue_declare(queue=queue_name, durable=True) 
-    # 'durable' makes the queue survive broker restarts
+channel.queue_declare(queue=queue_name, durable=True)
+# 'durable' makes the queue survive broker restarts
 
-#bind Error queue
-channel.queue_bind(exchange=exchangename, queue=queue_name, routing_key='*.error') 
-    # bind the queue to the exchange via the key
-    # any routing_key with two words and ending with '.error' will be matched
-    
+# bind Error queue
+channel.queue_bind(exchange=exchangename,
+                   queue=queue_name, routing_key='*.error')
+# bind the queue to the exchange via the key
+# any routing_key with two words and ending with '.error' will be matched
+
 
 ############   Activity_Log queue    #############
-#delcare Activity_Log queue
+# delcare Activity_Log queue
 queue_name = 'Activity_Log'
 channel.queue_declare(queue=queue_name, durable=True)
-    # 'durable' makes the queue survive broker restarts
+# 'durable' makes the queue survive broker restarts
 
-#bind Activity_Log queue
-channel.queue_bind(exchange=exchangename, queue=queue_name, routing_key='post.*') 
-    # bind the queue to the exchange via the key
-    # 'routing_key=#' => any routing_key would be matched
-    
+# bind Activity_Log queue
+channel.queue_bind(exchange=exchangename,
+                   queue=queue_name, routing_key='post.*')
+# bind the queue to the exchange via the key
+# 'routing_key=#' => any routing_key would be matched
+
 
 """
 This function in this module sets up a connection and a channel to a local AMQP broker,
 and declares a 'topic' exchange to be used by the microservices in the solution.
 """
+
+
 def check_setup():
-    # The shared connection and channel created when the module is imported may be expired, 
+    # The shared connection and channel created when the module is imported may be expired,
     # timed out, disconnected by the broker or a client;
     # - re-establish the connection/channel is they have been closed
     global connection, channel, hostname, port, exchangename, exchangetype
 
     if not is_connection_open(connection):
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port, heartbeat=3600, blocked_connection_timeout=3600))
+        connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host=hostname, port=port, heartbeat=3600, blocked_connection_timeout=3600))
     if channel.is_closed:
         channel = connection.channel()
-        channel.exchange_declare(exchange=exchangename, exchange_type=exchangetype, durable=True)
+        channel.exchange_declare(
+            exchange=exchangename, exchange_type=exchangetype, durable=True)
 
 
 def is_connection_open(connection):
