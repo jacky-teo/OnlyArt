a1593fee5538d91b81f3de9d94e522383aab4019
LIN SHUYI <shuyi.lin.2020@scis.smu.edu.sg>
2022-03-29 17:24:13 +0800

    Update notification.py

--- services/notification.py
+++ services/notification.py
@@ -70,7 +70,7 @@ def find_by_isbn13(telegramtag):
         }
     ), 404
 
-
+#for testing purposes only
 @app.route("/chat/<string:telegramtag>", methods=['POST'])
 def create_notif(telegramtag):
     if (Notification.query.filter_by(telegramtag=telegramtag).first()):
