06122aaee36236461d6741b35497f798b78de54a
erlynnehazey <erlynneong.2020@scis.smu.edu.sg>
2022-03-29 10:02:31 +0800

    login.html frontend and vue

--- scripts/login.js
+++ scripts/login.js
@@ -1,9 +1,28 @@
 const app = Vue.createApp({
     data() {
         return {
-            isUser: false,
+            isConsumer: false,
             isCreator: false,
-            
+            userType: ''
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+        //update user type when user declares account type
+        declareUser(userType) {
+            this.userType = userType;
+            if (userType == 'creator') {
+                this.isCreator = true;
+                this.isConsumer = false;
+
+            } else {    
+                this.isConsumer = true;
+                this.isCreator = false;
+            }
         }
     }
 })
+
+app.mount('#loginPage')
