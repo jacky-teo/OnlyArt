d053cb882b2fdf9f53c53b90c206861f8742bd49
shyatup <quahshyawei@gmail.com>
2022-04-02 03:10:25 +0800

    Add Vue navbar component

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,1 +1,39 @@
 // vue.js for universal navbar
+const app = Vue.createApp({
+
+})
+
+//navbar vue component
+app.component('navbar', {
+    data() {
+        return {
+            appName: 'OnlyFence'
+        }
+    },
+    computed: {
+        links() {
+            // href links at upload directory
+            if (window.location.href.includes("creator_gallery")) {
+                return {
+                    homeLink: "./"
+                }
+            }
+            else {
+                return {
+                    homeLink: ""
+                }
+            }
+        }
+    },
+    template: `<nav class="px-5 navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">OnlyFences</a>
+      <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" :href=links.homeLink>Home</a>
+          </li>
+        </ul>
+      </div>
+    </nav>`
+})
+app.mount("#compApp")
